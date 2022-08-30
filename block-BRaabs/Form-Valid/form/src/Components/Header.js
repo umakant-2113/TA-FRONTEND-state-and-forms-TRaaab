b46ecd3052e88fdfc618ca745074ad2ac1696a25
umakant-2113 <lodhiumakant800@gmail.com>
2022-08-30 15:13:13 +0530

    Completed

---
+++ block-BRaabs/Form-Valid/form/src/Components/Header.js
@@ -0,0 +1,12 @@
+const Header=()=>{
+    return (
+        <header className="header">
+        <h1>
+            📑 Learning About Forms
+        </h1>
+    <h3>Controlled vs Uncontrolled Component</h3>
+        </header>
+    )
+}
+
+export default Header;
---
+++ block-BRaabs/Form-Valid/form/src/index.js
@@ -0,0 +1,7 @@
+import React from "react"
+import  ReactDOM  from "react-dom"
+import App from "./Components/App"
+import "./StyleSheet/style.css"
+let root= document.getElementById("root");
+
+ReactDOM.render(<App/>,root)
